13ef70a4b936bda231aa006d72ab5349569d10e2
RmxhbmRyZQ <635818021@qq.com>
2022-03-08 22:27:57 +0800

    modify README.md

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,22 +1,17 @@
-import cn.flandre.lotus.socket.stream.BlockOutputStream;
-import cn.flandre.lotus.socket.stream.FreeBlock;
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Node;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.WritableByteChannel;
 import java.util.Date;
-import java.util.Random;
-import java.util.UUID;
-import java.util.zip.GZIPOutputStream;
+import java.util.LinkedList;
 
 public class Test {
+    private Node head;
+
+    private void a(Node head) {
+
+    }
+
     public static void main(String[] args) throws IOException {
-        String toekn = String.valueOf(UUID.randomUUID());
         int breakPoint = 0;
     }
 }
